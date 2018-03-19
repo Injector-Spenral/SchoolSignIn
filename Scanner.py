7e4018e53a44a5cf4634e0131974da3f610c461b
Sam Richards <35410487+Injector-Spenral@users.noreply.github.com>
2018-03-19 06:32:01 +0000

    Initial Upload

---
+++ Scanner.py
@@ -0,0 +1,34 @@
+import csv
+import datetime
+import sys
+
+def importCSV():
+    #Import csv file into 'List' in and add the card to the dictionary for card scanning
+    Info = []
+    Dict = {'ListID' : 'CardID'}
+    with open('IDMap.csv', 'r') as csvfile:
+        spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+        i = 0
+        for row in spamreader:
+            Info.append(row)
+            Dict[row[0]] = i
+            i += 1
+    return Info, Dict
+        
+def scanCard(Info, Dict):
+    card = input('Please scan card')
+    fname = Info[Dict[card]][2]
+    lname =	Info[Dict[card]][3]
+    idnum = Info[Dict[card]][1]
+    print("Hello, " + fname + " " + lname + ", " + idnum)
+    return(card, idnum, fname, lname)
+
+
+def logAttendance(card, id, room):
+    with open('Log.csv', 'a') as csvfile:
+        spamwriter = csv.writer(csvfile, delimiter=' ', quotechar = '|')
+        spamwriter.writerow([datetime.datetime.isoformat(datetime.datetime.now()), card, id, room])
+
+Info, Dict = importCSV()
+card, idnum, fname, lname = scanCard(Info, Dict)
+logAttendance(card, idnum, sys.argv[1])
